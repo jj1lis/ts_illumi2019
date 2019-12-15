198722959b23a6d901202cc5ecf3fbba75c4d7fd
jj1lis <rinta.nambuline205@gmail.com>
2019-12-16 01:19:54 +0900

    [complete]:meybe...

--- ts_illumi2019.ino
+++ ts_illumi2019.ino
@@ -1,13 +1,14 @@
 #include <EEPROM.h>
+#include <avr/wdt.h>
 
 const int ADDR_MODE=0;              //EEPROMに書き込むモード用変数のアドレス
 const byte DUTY_MAX=100;            //PWMの周期。0より大きい範囲
-const byte DUTY_RATIO=50;          //PWMのDuty比。0-DUTY_MAXの間で設定
-const unsigned int FLASH_CYCLE=10000;//点滅周期(ミリ秒)
+const byte DUTY_RATIO=10;          //PWMのDuty比。0-DUTY_MAXの間で設定
+const unsigned int FLASH_CYCLE=2000;//点滅周期(ミリ秒)
 const byte OUT_MIN=2;               //出力ピンの一番下
 const byte OUT_MAX=13;              //出力ピンの一番上
-const byte SW_MODE=15;              //モード切替スイッチのピン。15? 16?
-const byte SW_MYST=16;              //もう一つの謎入力。要調査
+const char SW_MODE=A1;              //モード切替スイッチのピン。15? 16?
+const char SW_MYST=A2;              //もう一つの謎入力。要調査
 
 typedef enum{
     xmas=0,     //Fu*k X'mas
@@ -20,7 +21,7 @@ namespace Common{   //共通部分
         return 1.;
     }
     float pin3(float phase){
-        //return pow(sin(phrase),2)
+        //return pow(sin(phase),2)
         if(0<=phase&&phase<=PI){
             return 1;
         }else{
@@ -28,7 +29,7 @@ namespace Common{   //共通部分
         }
     }
     float pin4(float phase){
-        //return pow(-sin(phrase),2)
+        //return pow(-sin(phase),2)
         if(0<=phase&&phase<=PI){
             return 1;
         }else{
@@ -36,7 +37,7 @@ namespace Common{   //共通部分
         }
     }
     float pin5(float phase){
-        //return pow(sin(phrase),2)
+        //return pow(sin(phase),2)
         if(0<=phase&&phase<=PI){
             return 0;
         }else{
@@ -44,27 +45,35 @@ namespace Common{   //共通部分
         }
     }
     float pin6(float phase){
-        //return pow(-sin(phrase),2)
+        //return pow(-sin(phase),2)
         if(0<=phase&&phase<=PI){
             return 0;
         }else{
             return 1;
         }
     }
+    float pin7(float phase){
+        if(0<=phase&&phase<=PI){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    float pin8(float phase){
+        if(0<=phase&&phase<=PI){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
 
 namespace Xmas{     //クリスマス
-    float pin7(float phase){
-        return 1.;
-    }
-    float pin8(float phase){
-        return 1.;
-    }
     float pin9(float phase){
-        return 0.;
+        return 1.;
     }
     float pin10(float phase){
-        return 0.;
+        return 1.;
     }
     float pin11(float phase){
         return 0.;
@@ -78,23 +87,17 @@ namespace Xmas{     //クリスマス
 }
 
 namespace Mochi{    //正月
-    float pin7(float phase){
-        return 0.;
-    }
-    float pin8(float phase){
-        return 0.;
-    }
     float pin9(float phase){
         return 0.;
     }
     float pin10(float phase){
-        return 1.;
+        return 0.;
     }
     float pin11(float phase){
         return 0.;
     }
     float pin12(float phase){
-        return 0.;
+        return 1.;
     }
     float pin13(float phase){
         return 0.;
@@ -102,20 +105,14 @@ namespace Mochi{    //正月
 }
 
 namespace Oni{      //節分
-    float pin7(float phase){
-        return 1.;
-    }
-    float pin8(float phase){
-        return 0.;
-    }
     float pin9(float phase){
-        return 0.;
+        return 1.;
     }
     float pin10(float phase){
         return 0.;
     }
     float pin11(float phase){
-        return 0.;
+        return 1.;
     }
     float pin12(float phase){
         return 0.;
@@ -154,8 +151,11 @@ float (*getFunc(Mode mode,byte pin_num))(float){
 }
 
 void resetSoftware(){
-    Serial.println("DEBUG:reset...");
-    asm volatile("jmp 0");
+    Serial.println("reset");
+    wdt_disable();
+    wdt_enable(WDTO_15MS);
+    while(1){}
+    //asm volatile("jmp 0");
 }
 
 class Light{
@@ -168,13 +168,25 @@ class Light{
             mode=m;
             switch(mode){
                 case xmas:
+                    Serial.println("xmas");
+                    for(byte cnt=0;cnt<12;cnt++){
+                        pins[cnt]=getFunc(mode,cnt);
+                    }
+                    break;
                 case mochi:
+                    Serial.println("mochi");
+                    for(byte cnt=0;cnt<12;cnt++){
+                        pins[cnt]=getFunc(mode,cnt);
+                    }
+                    break;
                 case oni:
+                    Serial.println("oni");
                     for(byte cnt=0;cnt<12;cnt++){
                         pins[cnt]=getFunc(mode,cnt);
                     }
                     break;
                 default:
+                    EEPROM.write(ADDR_MODE,xmas);
                     resetSoftware();
             }
         }
@@ -182,13 +194,12 @@ class Light{
         void pwmUpdate(byte pwm_cnt,float phase){
             float duty_pin=0.;
             if(pwm_cnt>DUTY_RATIO){
-                for(byte pin=OUT_MIN; pin<=OUT_MAX; pin++){
-                    digitalWrite(pin, LOW);
+                for(byte pin=OUT_MIN;pin<=OUT_MAX;pin++){
+                    digitalWrite(pin,LOW);
                 }
             }else{
                 for(byte cnt=0;cnt<12;cnt++){
-                    duty_pin=pins[cnt](phase);
-                    if(pwm_cnt>(byte)DUTY_RATIO*duty_pin){
+                    if((float)pwm_cnt>=(float)DUTY_RATIO*pins[cnt](phase)){
                         digitalWrite(cnt+OUT_MIN,LOW);
                     }else{
                         digitalWrite(cnt+OUT_MIN,HIGH);
@@ -204,25 +215,24 @@ class Light{
 
 void setup(){
     Serial.begin(9600);
+    Serial.println("setup");
     for(int cnt=OUT_MIN; cnt<=OUT_MAX; cnt++){
         pinMode(cnt,OUTPUT);
     }
     pinMode(SW_MODE,INPUT);
     pinMode(SW_MYST,INPUT);
+    delay(1000);
 }
 
 void loop(){
+    Serial.println("loop");
     Light light((Mode)EEPROM.read(ADDR_MODE));
     float phase_now=0;
 
     while(1){
         if(digitalRead(SW_MODE)==HIGH){
-            while(digitalRead(SW_MODE)==HIGH){
-                if(digitalRead(SW_MODE)==LOW){
                     EEPROM.write(ADDR_MODE,light.getMode()+1);
                     resetSoftware();
-                }
-            }
         }
 
         phase_now=(float)(millis()%FLASH_CYCLE)/(float)FLASH_CYCLE*2.*PI;
